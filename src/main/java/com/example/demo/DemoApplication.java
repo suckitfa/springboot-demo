466c3cb6762d56d199fd4987ab54513dc3537c6d
suckitfa <254202042@qq.com>
2022-11-21 09:58:45 +0800

    feat: json arr & hello api

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,32 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@GetMapping
+	public String myhello() {
+		return "Hell World!";
+	}
+
+	@GetMapping("/list")
+	public List<String> hello() {
+		List<String> res = new ArrayList<>();
+		res.add("hello");
+		res.add("world");
+		return res;
+	}
+
 
 }
